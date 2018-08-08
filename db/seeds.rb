bb7e70e3ecb1c398d52f81afc5b839d3075506bc
Julian Schleiminger <j.schleiminger@gmail.com>
2018-08-08 14:43:32 +0200

    review and add restaurants to your hearts content

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Name.first_name,
+    address: Faker::Address.full_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+  restaurant.save!
+end
+
+5.times do
+  review = Review.new(
+    content: Faker::TwinPeaks.quote,
+    rating: [0, 1, 2, 3, 4, 5].sample,
+    restaurant_id: [0, 1, 2, 3, 4, 5].sample
+  )
+  review.save!
+end
